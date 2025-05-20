b10a7662384ae652634034e684dcb50b86ecc225
Harriet Owen <harriet.owen@theinformationlab.com>
2025-05-20 19:54:37 +0000

    int_orders_pivoted created using jinja

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,9 @@
 select
     orderid as order_id,
     status,
+    PAYMENTMETHOD as payment_method,
     sum(amount) / 100.0 as amount,
     max(created) as payment_finalized_date
 
 from {{ source('stripe', 'payment') }}
-group by 1,2
+group by 1,2,3
